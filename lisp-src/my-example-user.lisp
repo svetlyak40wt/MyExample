846c56f73ae56536c9d25d6da529682c232e63c4
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-11-14 12:21:46 +0300

    Generate widget content with Spinneret.

--- lisp-src/my-example-user.lisp
+++ lisp-src/my-example-user.lisp
@@ -11,14 +11,18 @@
 
 
 (setf (ningle:route *app* "/")
-      "<html>
-  <body>
-    <div style=\"width: 300px; height: 200px; background: #F55;\">
-      Hello World!
-    </div>
-  </body>
-</html>
-")
+      (spinneret:with-html-string
+        (:html
+         (:body :style "margin: 0px"
+          (:div :style "width: 100%; height: 200px; background: #F55;"
+                (:div :style "background: white; width: 100%; height: 100%; padding: 5px; margin-left: 5px; margin-top: 5px;"
+                 (:p "Hello from Hunchentoot!")
+                 (:dl
+                  (:dt "Lisp implementation")
+                  (:dd (lisp-implementation-type))
+                  (:dt "Lisp version")
+                  (:dd (lisp-implementation-version)))))))))
+
 
 (defun start-server (&key (port 10080))
   (clack:clackup *app*
