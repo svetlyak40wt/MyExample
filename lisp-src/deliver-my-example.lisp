b9c353f25794457c498528c04c669b182a68db5e
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-11-14 12:06:32 +0300

    Working app.

--- lisp-src/deliver-my-example.lisp
+++ lisp-src/deliver-my-example.lisp
@@ -2,11 +2,17 @@
 
 (asdf:load-system :ningle)
 (asdf:load-system :clack)
-(asdf:load-system :clack-handler-toot)
+(asdf:load-system :clack-handler-hunchentoot)
 
 
 (compile-file "lisp-src/my-example-user.lisp" :load :delete :output-file :temp)
 
-(deliver-to-android-project nil "./" 5)
+(deliver-to-android-project nil "./"
+                            ;; delivery level
+                            0
+                            ;; With this level app does not work :(
+                            ;; 5
+                            ;; Without this lack-middleware-backtrace can't be loaded
+                            :keep-eval t)
 
 (quit)
--- lisp-src/my-example-user.lisp
+++ lisp-src/my-example-user.lisp
@@ -1,24 +1,39 @@
 (in-package "CL-USER")
 
 (export 'get-url-to-display)
-(declaim (notinline get-url-to-display))
+(export 'start-server)
 
-(defun get-url-to-display ()
-  "https://svetlyak.ru")
+(declaim (notinline get-url-to-display
+                    start-server))
 
 
 (defvar *app* (make-instance 'ningle:app))
 
 
 (setf (ningle:route *app* "/")
-      "Welcome to ningle!")
-
-
-(export 'start-server)
-(declaim (notinline start-server))
+      "<html>
+  <body>
+    <div style=\"width: 300px; height: 200px; background: #F55;\">
+      Hello World!
+    </div>
+  </body>
+</html>
+")
 
 (defun start-server (&key (port 10080))
-  (clack:clackup *app* :port port :server :toot))
+  (clack:clackup *app*
+                 :port port :server :hunchentoot))
+
+
+(defun get-url-to-display ()
+  (handler-bind ((error (lambda (c)
+                          (return-from get-url-to-display
+                            (format nil "Traceback: ~A" c))) ))
+    (start-server)
+    (sleep 10)
+    (values "http://127.0.0.1:10080/")
+    ;; (values "https://svetlyak.ru/")
+    ))
 
 
 (deliver-keep-symbols 'get-url-to-display 'start-server)
