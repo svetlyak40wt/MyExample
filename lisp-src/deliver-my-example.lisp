4b56ec8b17cc26d0a43847d290e71efc011d08b4
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-11-14 19:49:51 +0300

    Build with Weblocks.

--- lisp-src/deliver-my-example.lisp
+++ lisp-src/deliver-my-example.lisp
@@ -1,10 +1,12 @@
 (load "lisp-src/bundle/bundle.lisp")
 
+(push :dexador-no-ssl
+      *features*)
+
 (asdf:load-system :ningle)
 (asdf:load-system :clack)
-(asdf:load-system :spinneret)
+(asdf:load-system :weblocks)
 (asdf:load-system :clack-handler-hunchentoot)
-
 
 (compile-file "lisp-src/my-example-user.lisp" :load :delete :output-file :temp)
 
