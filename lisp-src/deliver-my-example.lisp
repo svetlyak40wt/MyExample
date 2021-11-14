59d0ec851c1dfa6162512b271c1412a9cdbc456c
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-11-15 00:04:27 +0300

    Solved encoding issues when loading Parenscript.

--- lisp-src/deliver-my-example.lisp
+++ lisp-src/deliver-my-example.lisp
@@ -5,6 +5,21 @@
 
 (push "lisp-src/" asdf:*central-registry*)
 
+(defun force-utf-8-file-encoding (pathname ef-spec buffer length)
+  (declare (ignore pathname buffer length))
+  ;; (system:merge-ef-specs ef-spec :utf-8)
+  (system:merge-ef-specs ef-spec '(:utf-8 :eol-style :lf)))
+
+(set-default-character-element-type 'character)
+
+;; This will change how encoding for all loaded lisp
+;; files is detected.
+(setf system:*file-encoding-detection-algorithm*
+      '(force-utf-8-file-encoding))
+
+(setf stream::*default-external-format* '(:utf-8 :eol-style :lf))
+
+
 (asdf:load-system :todo)
 
 ;; (asdf:load-system :ningle)
@@ -13,6 +28,7 @@
 ;; (asdf:load-system :clack-handler-hunchentoot)
 
 (compile-file "lisp-src/my-example-user.lisp" :load :delete :output-file :temp)
+
 
 (deliver-to-android-project nil "./"
                             ;; delivery level
